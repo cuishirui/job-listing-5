3f5ec757452451a30e333a7a25a5f891229df9fc
cuishirui <344494024@qq.com>
2017-01-25 14:01:09 +0800

    add more cloumn

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,4 +1,6 @@
 class Admin::JobsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_is_admin
 
   def index
       @jobs = Job.all
@@ -40,8 +42,15 @@ class Admin::JobsController < ApplicationController
     redirect_to admin_jobs_path
   end
 
+  def require_is_admin
+    if !current_user.admin?
+      flash[:alert] = "You are not admin"
+      redirect_to root_path
+    end
+  end
+
   private
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
   end
 end
